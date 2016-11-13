8793e69653da98d637919d42326cf2fabe99efee
nivethavadivelu <nivethavadivelu@gmail.com>
2016-11-12 19:13:10 -0600

    A proper form validation and rendering of the requested query added.

--- queryprocessor/userinteraction/views.py
+++ queryprocessor/userinteraction/views.py
@@ -4,15 +4,21 @@ from forms import UserQuery
 
 
 def get_query_form(request):
+    # When the form is generated for the first time, it has a GET request
+    return render(request, 'userinteraction/user_query_input.html', {
+        'form': UserQuery(request.GET)
+    })
+
+def get_results(request):
+    # Checking if the form is correct and then proceeding
     if request.method == 'POST':
         form = UserQuery(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/result/')
+            return render(request, 'userinteraction/result.html', {
+                'query': form.cleaned_data['query']
+            })
     else:
         form = UserQuery(request.GET)
     return render(request, 'userinteraction/user_query_input.html', {
         'form': form
     })
-
-def get_results(request):
-    return render(request, 'userinteraction/result.html')

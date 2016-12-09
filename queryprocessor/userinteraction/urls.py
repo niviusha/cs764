909f8a3c939de065e609ded030d9c9ef3217cd65
nivethavadivelu <nivethavadivelu@gmail.com>
2016-12-08 20:15:53 -0600

    Front-end interactive part setup

--- queryprocessor/userinteraction/urls.py
+++ queryprocessor/userinteraction/urls.py
@@ -4,4 +4,5 @@ import views
 urlpatterns = [
     url(r'^result', views.get_results, name="result"),
     url(r'^user_query_input', views.get_query_form, name="user_query_input"),
+    url(r'^train', views.train_system, name="train"),
 ]
--- queryprocessor/userinteraction/views.py
+++ queryprocessor/userinteraction/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from forms import UserQuery
+from forms import UserQuery, ReformQuery
 
 
 def get_query_form(request):
@@ -15,10 +15,16 @@ def get_results(request):
         form = UserQuery(request.POST)
         if form.is_valid():
             return render(request, 'userinteraction/result.html', {
-                'query': form.cleaned_data['query']
+                'query': form.cleaned_data['query'],
+                'form': ReformQuery(request.GET)
             })
     else:
         form = UserQuery(request.GET)
-    return render(request, 'userinteraction/user_query_input.html', {
-        'form': form
-    })
+    return redirect("/user_query_input")
+
+def train_system(request):
+  if request.method == 'POST':
+    form = ReformQuery(request.POST)
+    if form.is_valid():
+      correct_query = form.cleaned_data['query']
+  return redirect("/user_query_input")
